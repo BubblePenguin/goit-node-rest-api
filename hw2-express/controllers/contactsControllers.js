ecde8709633bb0d9907fb7c04319914921ac2940
BubblePenguin <kotov.senpai@gmail.com>
2024-02-25 06:16:59 +0200

    0 testing, need some rest, not sure if it even works, i'll get back to task tomorow

--- hw2-express/controllers/contactsControllers.js
+++ hw2-express/controllers/contactsControllers.js
@@ -4,13 +4,42 @@ import {
   removeContact,
   addContact,
 } from "../services/contactsServices.js";
+import { HttpError } from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = (req, res) => {
+  res.json(listContacts());
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = getContactById(id);
+    if (!result) throw HttpError(404, "Not found");
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = getContactById(id);
+    if (!result) throw HttpError(404, "Not found");
+    res.json(removeContact(id));
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = (req, res) => {
+  try {
+    const { name, phone, email } = req.params;
+    if (!name || !phone || !email) throw HttpError(403, "Not enough data");
+    res.json(addContact({ name, phone, email }));
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const updateContact = (req, res) => {};
